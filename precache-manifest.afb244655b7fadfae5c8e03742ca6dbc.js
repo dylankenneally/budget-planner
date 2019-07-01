4bfb893d13297379b6ebdba5fb49d2da0a4d7d1d
Dylan Kenneally <dylan.kenneally@gmail.com>
2019-07-01 22:40:44 +1000

    Updates

---
+++ precache-manifest.afb244655b7fadfae5c8e03742ca6dbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9658eaf3cdcde3134a9ca1a4344d0f",
+    "url": "/budget-planner/index.html"
+  },
+  {
+    "revision": "7bc46cbaa09075ebe821",
+    "url": "/budget-planner/static/css/main.90b74262.chunk.css"
+  },
+  {
+    "revision": "f29c3faee38e6962cc3c",
+    "url": "/budget-planner/static/js/2.35ff0048.chunk.js"
+  },
+  {
+    "revision": "7bc46cbaa09075ebe821",
+    "url": "/budget-planner/static/js/main.b4c29381.chunk.js"
+  },
+  {
+    "revision": "6363082918b0fc50d71e",
+    "url": "/budget-planner/static/js/runtime~main.8ec2c2e1.js"
+  }
+]);
