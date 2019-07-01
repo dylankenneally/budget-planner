b2f9c6221842d6b7133dd300e704e19a39a3bb93
Dylan Kenneally <dylan.kenneally@gmail.com>
2019-07-01 20:58:57 +1000

    intro header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import {
 	ExpansionPanel,
 	ExpansionPanelDetails,
 	ExpansionPanelSummary,
+	Grid,
 	Typography,
 } from '@material-ui/core';
 import { ExpandMore as ExpandMoreIcon } from '@material-ui/icons';
@@ -12,6 +13,29 @@ import store from '../dataModel/dataStore';
 import PeriodSelector from './PeriodSelector';
 import CategoryGroup from './CategoryGroup';
 import ResultsChart from './ResultsChart';
+import './App.css';
+
+const Header = () => {
+	const { t } = useTranslation();
+	const [period, setPeriod] = useState(store.get('summary-period'));
+	store.subscribe('summary-period', (p) => setPeriod(p));
+
+	return (
+		<Grid container spacing={4}>
+			<Grid item xs={8}>
+				<Typography variant="h6" gutterBottom>
+					{t('app-title')}
+				</Typography>
+			</Grid>
+			<Grid item xs={4}>
+				<div className="header-text">
+					{t('view-as')}&nbsp;
+					<PeriodSelector period={period} onChange={newPeriod => store.set('summary-period', newPeriod) } />
+				</div>
+			</Grid>
+		</Grid>
+	);
+};
 
 const App = () => {
 	const [budget] = useState(store.get('budget'));
@@ -33,12 +57,7 @@ const App = () => {
 
 	return (
 		<>
-			<Typography variant="h5" gutterBottom>
-				{t('dev.todo')}
-			</Typography>
-
-			<PeriodSelector period={mainPeriod} onChange={newPeriod => store.set('summary-period', newPeriod) } />
-
+			<Header />
 			{catElements}
 
 			<ExpansionPanel defaultExpanded>
