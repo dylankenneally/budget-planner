98a7fc88946f0bfeceee2982f4c4f1a27da893e7
Dylan Kenneally <dylan.kenneally@gmail.com>
2019-07-01 20:03:27 +1000

    store app period

--- src/dataModel/dataProvider.js
+++ src/dataModel/dataProvider.js
@@ -1,17 +1,24 @@
 // Data provider
 import store from './dataStore';
 import defaultBudget from '../data/defaultBudget';
+import Periods from './periods';
 
 store.set('budget', defaultBudget);
 defaultBudget.forEach(category => {
 	category.entries.forEach(entry => {
 		const id = category.id + entry.id;
-		let amount = window.localStorage.getItem(id);
-		if (amount) {
-			entry.amount = parseInt(amount, 10);
+		let loadedEntry = window.localStorage.getItem(id);
+		if (loadedEntry) {
+			loadedEntry = JSON.parse(loadedEntry);
+			entry.amount = loadedEntry.amount;
+			entry.period = loadedEntry.period;
 		}
 
 		store.set(id, entry);
-		store.subscribe(id, (e) => window.localStorage.setItem(id, e.amount));
+		store.subscribe(id, (e) => window.localStorage.setItem(id, JSON.stringify(e)));
 	});
 });
+
+let summaryPeriod = window.localStorage.getItem('summary-period') || Periods.annually;
+store.set('summary-period', summaryPeriod);
+store.subscribe('summary-period', (p) => window.localStorage.setItem('summary-period', p));
